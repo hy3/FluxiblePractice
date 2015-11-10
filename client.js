9555507ecb5b69d3bcaf781b7b11b588d8ce616f
hy3 <atelier.hy3@gmail.com>
2015-11-10 14:52:26 +0900

    Fix client script.
    To indicate correct DOM id, and remove unnecessary process.

--- client.js
+++ client.js
@@ -2,20 +2,11 @@
 
 import ReactDOM from 'react-dom';
 import React from 'react';
-import debug from 'debug';
 import { createElementWithContext } from 'fluxible-addons-react';
 import app from './app';
 
-const debugClient = debug('fluxiblepractice');
 const dehydratedState = window.App; // Sent from the server
 
-window.React = ReactDOM; // For chrome dev tool support
-
-// expose debug object to browser, so that it can be enabled/disabled from browser:
-// https://github.com/visionmedia/debug#browser-support
-window.fluxibleDebug = debug;
-
-debugClient('rehydrating app');
 
 // pass in the dehydrated server state from server.js
 app.rehydrate(dehydratedState, (err, context) => {
@@ -23,12 +14,10 @@ app.rehydrate(dehydratedState, (err, context) => {
         throw err;
     }
     window.context = context;
-    const mountNode = document.getElementById('app');
+    const mountNode = document.getElementById('todoapp');
 
-    debugClient('React Rendering');
     ReactDOM.render(
         createElementWithContext(context),
-        mountNode,
-        () => debugClient('React Rendered')
+        mountNode
     );
 });
